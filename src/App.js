b90d99efe10f2d65b48ba35ca9b46d56d82421a3
Kaleb Tessema <69762435+Spongebotter@users.noreply.github.com>
2022-06-30 20:27:31 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent/>
+      <ul>
+        <li>List element 1 </li>
+        <li>List element 2 </li>
+      </ul>
+      
+    </div>
+    
+  );
+}
+
+
+export default App;
